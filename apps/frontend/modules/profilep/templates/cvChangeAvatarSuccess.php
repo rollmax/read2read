2ee52aa7a9e60a50f2433704a8dafe82ba394824
Aleksey Khoroshev <aleksxor@gmail.com>
2012-10-24 18:38:30 +0700

    read2read-40 Проблема с размером изображения (фото) в резюме

--- apps/frontend/modules/profilep/templates/cvChangeAvatarSuccess.php
+++ apps/frontend/modules/profilep/templates/cvChangeAvatarSuccess.php
@@ -4,7 +4,6 @@
       <?php echo $form->renderHiddenFields(); ?>
       <?php echo $form['avatar']->renderError(); ?>
       <?php echo $form['avatar']->render(); ?>
-      <p style="margin-top:7px;">максимальный размер изображения 240х400 пикселей</p>
       <input type="submit" value="Изменить" style="float: right; margin-right: 110px;" />
     </form>
   </td>
--- lib/form/UserAvatarForm.class.php
+++ lib/form/UserAvatarForm.class.php
@@ -1,44 +1,51 @@
 <?php
 class UserAvatarForm extends UserForm
 {
-  public function  configure()
-  {
-    $this->useFields(array('avatar'));
-    $this->widgetSchema['avatar'] = new sfWidgetFormInputFile();
-
-    $this->validatorSchema['avatar'] = new sfValidatorFile(array(
-      'required'   => false,
-      'path'       => sfConfig::get('sf_upload_dir').sfConfig::get('app_avatars_path_dir'),
-      'mime_types' => 'web_images',
-    ));
-  }
-
-  
-  public function processValues($values)
-  {
-    if ($values['avatar'] instanceof sfValidatedFile)
-    { // file was uploaded
-      $path = $this->getObject()->checkDir($values['avatar']->getPath(), $this->getObject()->getId());
-      $values['avatar'] = $this->processPicture($values['avatar'], $path, 240, 400);
-    }
-    unset($values['avatar']);
-    return parent::processValues($values);
-  }
-
-  protected function processPicture(sfValidatedFile $file, $path, $width, $height, $crop = false)
-  {
-    if(!$image = $this->getObject()->savePicture($file, $path, $width, $height, $crop))
-      return false;
-    $user = $this->getObject();
-    if($user->getAvatar() !== null)
+    public function  configure()
     {
-      if(!unlink(sfConfig::get('sf_upload_dir').sfConfig::get('app_avatars_path_dir').$user->getAvatar()))
-        return false;
+        $this->useFields(array('avatar'));
+        $this->widgetSchema['avatar'] = new sfWidgetFormInputFile();
+
+        $this->validatorSchema['avatar'] = new sfValidatorFile(array(
+            'required' => false,
+            'path' => sfConfig::get('sf_upload_dir') . sfConfig::get('app_avatars_path_dir'),
+            'mime_types' => 'web_images',
+        ));
     }
 
-    $imageLocation = str_replace(sfConfig::get('sf_upload_dir').sfConfig::get('app_avatars_path_dir'), '', $image);
-    $user->setAvatar($imageLocation);
-    $user->save();
-        
-  }
+
+    public function processValues($values)
+    {
+
+        if ($values['avatar'] instanceof sfValidatedFile) { // file was uploaded
+            $path = $this->getObject()->checkDir($values['avatar']->getPath(), $this->getObject()->getId());
+            $values['avatar'] = $this->processPicture($values['avatar'], $path, 20000, 40000, false);
+        }
+        unset($values['avatar']);
+
+
+        return parent::processValues($values);
+    }
+
+    protected function processPicture(sfValidatedFile $file, $path, $width, $height, $crop = false)
+    {
+        if (!$image = $this->getObject()->savePicture($file, $path, $width, $height, $crop)) {
+            return false;
+        }
+        $user = $this->getObject();
+        if ($user->getAvatar() !== null) {
+            if (!unlink(sfConfig::get('sf_upload_dir') . sfConfig::get('app_avatars_path_dir') . $user->getAvatar())) {
+                return false;
+            }
+        }
+
+        $imageLocation = str_replace(
+            sfConfig::get('sf_upload_dir') . sfConfig::get('app_avatars_path_dir'),
+            '',
+            $image
+        );
+        $user->setAvatar($imageLocation);
+        $user->save();
+
+    }
 }
