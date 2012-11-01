e1e9c4d1ed25e2f714cc2f11b545d0b1fa56b850
Aleksey Khoroshev <aleksxor@gmail.com>
2012-11-01 11:48:36 +0700

    read2read-57 Отображение интерфейса оценки статьи со смещением.

--- apps/frontend/modules/category/templates/showArticleSuccess.php
+++ apps/frontend/modules/category/templates/showArticleSuccess.php
@@ -69,181 +69,184 @@
     </td>
     <td class=center>
         <?php if ($sf_user->isAuthenticated()): ?>
-        <?php if ($sf_user->getGuardUser()->isPurchaseArticle($article->getId())): ?>
+            <?php if ($sf_user->getGuardUser()->isPurchaseArticle($article->getId())): ?>
 
-            <?php if (false === $sf_user->getGuardUser()->hasRateContent(
-                $article->getId()
-            ) || false === $sf_user->getGuardUser()->hasRateTranslate($article->getId())
-            ): ?>
-                <p class=lt>Оценить</p>
-                <p class=rt>&nbsp;&nbsp;&nbsp;&nbsp;</p>
+                <?php if (false === $sf_user->getGuardUser()->hasRateContent(
+                    $article->getId()
+                ) || false === $sf_user->getGuardUser()->hasRateTranslate($article->getId())
+                ): ?>
+                    <p class=lt>Оценить</p>
+                    <p class=rt>&nbsp;&nbsp;&nbsp;&nbsp;</p>
                 <?php else: ?>
-                <p class=lt>Рэйтинг</p>
-                <p class=rt>&nbsp;&nbsp;&nbsp;&nbsp;</p>
+                    <p class=lt>Рэйтинг</p>
+                    <p class=rt>&nbsp;&nbsp;&nbsp;&nbsp;</p>
                 <?php endif; ?>
 
-            <?php if (false === $sf_user->getGuardUser()->hasRateContent($article->getId())): ?>
-                <p class=lt_ev>Статью</p>
-                <select name="art_rating" class="rating" id="art_rating">
-                    <option value="0">0</option>
-                    <option value="1">one | один</option>
-                    <option value="2">two | два</option>
-                    <option value="3">three | три</option>
-                    <option value="4">four | четыре</option>
-                    <option value="5">five | пять</option>
-                </select>
+                <?php if (false === $sf_user->getGuardUser()->hasRateContent($article->getId())): ?>
+                    <p class=lt_ev>Статью</p>
+                    <select name="art_rating" class="rating" id="art_rating">
+                        <option value="0">0</option>
+                        <option value="1">one | один</option>
+                        <option value="2">two | два</option>
+                        <option value="3">three | три</option>
+                        <option value="4">four | четыре</option>
+                        <option value="5">five | пять</option>
+                    </select>
 
-                <div class="rateit" data-rateit-backingfld="#art_rating" data-rateit-resetable="false"
-                     id="art_rating_div_user"></div>
+                    <div class="rateit" data-rateit-backingfld="#art_rating" data-rateit-resetable="false"
+                         id="art_rating_div_user"></div>
+                    <div style="clear: both;"></div>
                 <?php else: ?>
-                <p class=lt_ev>Оценка статьи</p>
-                <select name="art_rating" class="rating" id="art_rating_rated">
-                    <option value="0">0</option>
-                    <?php for ($i = 1; $i <= 5; $i++): ?>
-                    <option value="<?php echo $i ?>" <?php echo ($i == floor(
-                        $article->getContRate()
-                    )) ? 'selected' : '';?>>
-                        <?php echo $i ?>
-                    </option>
-                    <?php endfor;?>
-                </select>
+                    <p class=lt_ev>Оценка статьи</p>
+                    <select name="art_rating" class="rating" id="art_rating_rated">
+                        <option value="0">0</option>
+                        <?php for ($i = 1; $i <= 5; $i++): ?>
+                        <option value="<?php echo $i ?>" <?php echo ($i == floor(
+                            $article->getContRate()
+                        )) ? 'selected' : '';?>>
+                            <?php echo $i ?>
+                        </option>
+                        <?php endfor;?>
+                    </select>
 
-                <div class="rateit" data-rateit-backingfld="#art_rating_rated" data-rateit-resetable="false"
-                     data-rateit-readonly="true"></div>
+                    <div class="rateit" data-rateit-backingfld="#art_rating_rated" data-rateit-resetable="false"
+                         data-rateit-readonly="true"></div>
+                    <div style="clear: both;"></div>
                 <?php endif; ?>
 
-            <?php if (false === $sf_user->getGuardUser()->hasRateTranslate($article->getId())): ?>
-                <p class=lt_ev>Перевод</p>
-                <select name="trans_rating" class="rating2" id="trans_rating">
-                    <option value="0">0</option>
-                    <option value="1">one | один</option>
-                    <option value="2">two | два</option>
-                    <option value="3">three | три</option>
-                    <option value="4">four | четыре</option>
-                    <option value="5">five | пять</option>
-                </select>
+                <?php if (false === $sf_user->getGuardUser()->hasRateTranslate($article->getId())): ?>
+                    <p class=lt_ev>Перевод</p>
+                    <select name="trans_rating" class="rating2" id="trans_rating">
+                        <option value="0">0</option>
+                        <option value="1">one | один</option>
+                        <option value="2">two | два</option>
+                        <option value="3">three | три</option>
+                        <option value="4">four | четыре</option>
+                        <option value="5">five | пять</option>
+                    </select>
 
-                <div class="rateit" data-rateit-backingfld="#trans_rating" data-rateit-resetable="false"
-                     id="trans_rating_div_user"></div>
-                <?php else: ?>
-                <p class=lt_ev>Оценка перевода</p>
-                <select name="trans_rating" class="rating" id="trans_rating_rated">
-                    <option value="0">0</option>
-                    <?php for ($i = 1; $i <= 5; $i++): ?>
-                    <option value="<?php echo $i ?>" <?php echo ($i == floor(
-                        $article->getTransRate()
-                    )) ? 'selected' : '';?>>
-                        <?php echo $i ?>
-                    </option>
-                    <?php endfor;?>
-                </select>
+                    <div class="rateit" data-rateit-backingfld="#trans_rating" data-rateit-resetable="false"
+                         id="trans_rating_div_user"></div>
+                    <?php else: ?>
+                    <p class=lt_ev>Оценка перевода</p>
+                    <select name="trans_rating" class="rating" id="trans_rating_rated">
+                        <option value="0">0</option>
+                        <?php for ($i = 1; $i <= 5; $i++): ?>
+                        <option value="<?php echo $i ?>" <?php echo ($i == floor(
+                            $article->getTransRate()
+                        )) ? 'selected' : '';?>>
+                            <?php echo $i ?>
+                        </option>
+                        <?php endfor;?>
+                    </select>
 
-                <div class="rateit" data-rateit-backingfld="#trans_rating_rated" data-rateit-resetable="false"
-                     data-rateit-readonly="true"></div>
+                    <div class="rateit" data-rateit-backingfld="#trans_rating_rated" data-rateit-resetable="false"
+                         data-rateit-readonly="true"></div>
+                    <div style="clear: both;"></div>
                 <?php endif; ?>
 
-            <?php $sEnvironment = sfConfig::get('sf_environment') != 'prod' ? '/frontend_dev.php' : ''; ?>
+                <?php $sEnvironment = sfConfig::get('sf_environment') != 'prod' ? '/frontend_dev.php' : ''; ?>
 
-            <script type="text/javascript" src="/js/jquery.rateit.js"></script>
-            <script type="text/javascript">
-                $("#art_rating_div_user").bind('rated', function (event, value) {
-                    $.post(
-                            '<?php echo $sEnvironment?>/article/rate/<?php echo $article->getId()?>/content/' + value,
-                            function (data) {
-                                if (data.success == 1) {
-                                    $('#art_rating_div_user').attr("data-rateit-readonly", "true");
-                                    $('#art_rating_div_user .rateit-range').unbind();
-                                    return;
-                                }
-                                else
-                                    return;
-                            },
-                            "json"
-                    )
-                });
+                <script type="text/javascript" src="/js/jquery.rateit.js"></script>
+                <script type="text/javascript">
+                    $("#art_rating_div_user").bind('rated', function (event, value) {
+                        $.post(
+                                '<?php echo $sEnvironment?>/article/rate/<?php echo $article->getId()?>/content/' + value,
+                                function (data) {
+                                    if (data.success == 1) {
+                                        $('#art_rating_div_user').attr("data-rateit-readonly", "true");
+                                        $('#art_rating_div_user .rateit-range').unbind();
+                                        return;
+                                    }
+                                    else
+                                        return;
+                                },
+                                "json"
+                        )
+                    });
 
-                $("#trans_rating_div_user").bind('rated', function (event, value) {
-                    $.post(
-                            '<?php echo $sEnvironment?>/article/rate/<?php echo $article->getId()?>/translate/' + value,
-                            function (data) {
-                                if (data.success == 1) {
-                                    $('#trans_rating_div_user').attr("data-rateit-readonly", "true");
-                                    $('#trans_rating_div_user .rateit-range').unbind();
-                                    return;
-                                }
-                                else
-                                    return;
-                            },
-                            "json"
-                    )
-                });
-            </script>
-
+                    $("#trans_rating_div_user").bind('rated', function (event, value) {
+                        $.post(
+                                '<?php echo $sEnvironment?>/article/rate/<?php echo $article->getId()?>/translate/' + value,
+                                function (data) {
+                                    if (data.success == 1) {
+                                        $('#trans_rating_div_user').attr("data-rateit-readonly", "true");
+                                        $('#trans_rating_div_user .rateit-range').unbind();
+                                        return;
+                                    }
+                                    else
+                                        return;
+                                },
+                                "json"
+                        )
+                    });
+                </script>
             <?php else: ?>
-            <p class=lt>&nbsp;</p>
-            <p class=rt>&nbsp;</p>
+                <p class=lt>&nbsp;</p>
+                <p class=rt>&nbsp;</p>
             <?php endif; ?>
         <?php endif; ?>
 
         <!-- admin rating   -->
         <?php if ($sf_user->isAuthenticated() && $sf_user->hasPermission('admin')): ?>
-        <p class=lt>Read2Read</p>
-        <p class=rt>&nbsp;&nbsp;&nbsp;&nbsp;</p>
+            <p class=lt>Read2Read</p>
+            <p class=rt>&nbsp;&nbsp;&nbsp;&nbsp;</p>
 
-        <p class=lt_ev>Статья</p>
-        <select name="art_rating" class="rating" id="art_rating2">
-            <option value="0">0</option>
-            <?php for ($i = 1; $i <= 5; $i++): ?>
-            <option value="<?php echo $i ?>" <?php echo ($i == floor($article->getR2rContRate())) ? 'selected' : '';?>>
-                <?php echo $i ?>
-            </option>
-            <?php endfor;?>
-        </select>
+            <p class=lt_ev>Статья</p>
+            <select name="art_rating" class="rating" id="art_rating2">
+                <option value="0">0</option>
+                <?php for ($i = 1; $i <= 5; $i++): ?>
+                <option value="<?php echo $i ?>" <?php echo ($i == floor($article->getR2rContRate())) ? 'selected' : '';?>>
+                    <?php echo $i ?>
+                </option>
+                <?php endfor;?>
+            </select>
 
-        <div class="rateit" data-rateit-backingfld="#art_rating2" data-rateit-resetable="false"
-             id="art_rating_div"></div>
+            <div class="rateit" data-rateit-backingfld="#art_rating2" data-rateit-resetable="false"
+                 id="art_rating_div"></div>
+            <div style="clear: both;"></div>
 
-        <p class=lt_ev>Перевод</p>
-        <select name="cont_rating" class="rating" id="trans_rating2">
-            <option value="0">0</option>
-            <?php for ($i = 1; $i <= 5; $i++): ?>
-            <option value="<?php echo $i ?>" <?php echo ($i == floor($article->getR2rTransRate())) ? 'selected' : '';?>>
-                <?php echo $i ?>
-            </option>
-            <?php endfor;?>
-        </select>
+            <p class=lt_ev>Перевод</p>
+            <select name="cont_rating" class="rating" id="trans_rating2">
+                <option value="0">0</option>
+                <?php for ($i = 1; $i <= 5; $i++): ?>
+                <option value="<?php echo $i ?>" <?php echo ($i == floor($article->getR2rTransRate())) ? 'selected' : '';?>>
+                    <?php echo $i ?>
+                </option>
+                <?php endfor;?>
+            </select>
 
-        <div class="rateit" data-rateit-backingfld="#trans_rating2" data-rateit-resetable="false"
-             id="trans_rating_div"></div>
+            <div class="rateit" data-rateit-backingfld="#trans_rating2" data-rateit-resetable="false"
+                 id="trans_rating_div"></div>
+            <div style="clear: both;"></div>
 
-        <?php $sEnvironment = sfConfig::get('sf_environment') != 'prod' ? '/frontend_dev.php' : ''; ?>
+            <?php $sEnvironment = sfConfig::get('sf_environment') != 'prod' ? '/frontend_dev.php' : ''; ?>
 
-        <script type="text/javascript" src="/js/jquery.rateit.js"></script>
-        <script type="text/javascript">
-            $("#art_rating_div").bind('rated', function (event, value) {
-                $.post(
-                        '<?php echo $sEnvironment?>/article/rate/<?php echo $article->getId()?>/r2rcontent/' + value,
-                        function (data) {
-                            return;
-                        },
-                        "json"
-                )
-            });
+            <script type="text/javascript" src="/js/jquery.rateit.js"></script>
+            <script type="text/javascript">
+                $("#art_rating_div").bind('rated', function (event, value) {
+                    $.post(
+                            '<?php echo $sEnvironment?>/article/rate/<?php echo $article->getId()?>/r2rcontent/' + value,
+                            function (data) {
+                                return;
+                            },
+                            "json"
+                    )
+                });
 
-            $("#trans_rating_div").bind('rated', function (event, value) {
-                $.post(
-                        '<?php echo $sEnvironment?>/article/rate/<?php echo $article->getId()?>/r2rtranslate/' + value,
-                        function (data) {
-                            return;
-                        },
-                        "json"
-                )
-            });
-        </script>
-
+                $("#trans_rating_div").bind('rated', function (event, value) {
+                    $.post(
+                            '<?php echo $sEnvironment?>/article/rate/<?php echo $article->getId()?>/r2rtranslate/' + value,
+                            function (data) {
+                                return;
+                            },
+                            "json"
+                    )
+                });
+            </script>
         <?php else: ?>
-        <p class=lt>&nbsp;</p>
-        <p class=rt>&nbsp;</p>
+            <p class=lt>&nbsp;</p>
+            <p class=rt>&nbsp;</p>
         <?php endif;?>
         <!-- end admin rating   -->
 
