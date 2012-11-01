b8b37b844d83a4e342a60a5714921bb316cafd49
Aleksey Khoroshev <aleksxor@gmail.com>
2012-11-01 12:55:09 +0700

    read2read-56 Ширина поля для добавления комента

--- apps/frontend/modules/article/templates/commentSuccess.php
+++ apps/frontend/modules/article/templates/commentSuccess.php
@@ -4,13 +4,13 @@
       <form id="commentForm-<?php echo $form->getObject()->getId(); ?>" method="post" action="">
         <table>
           <tr>
-            <td>
+            <td class="comment">
               <?php echo $form->renderHiddenFields(); ?>
               <a class="saveCmt" onclick="article.saveComment('commentForm-<?php echo $form->getObject()->getId(); ?>', '<?php echo url_for('comment_update', $form->getObject()); ?>')">Сохранить</a>
               <p><?php echo $form['comment_en']->renderError(); ?></p>
               <p><?php echo $form['comment_en']->render(); ?></p>
             </td>
-            <td>
+            <td class="comment">
               <a class=del onclick="article.deleteComment('comment-<?php echo $form->getObject()->getId(); ?>', '<?php echo url_for('comment_delete', $form->getObject()); ?>')">Удалить</a>
               <p><?php echo $form['comment_ru']->renderError(); ?></p>
               <p><?php echo $form['comment_ru']->render(); ?></p>
