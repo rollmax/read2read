4b0086c4988b2453f4ca8827cf78b8a515231c96
Aleksey Khoroshev <aleksxor@gmail.com>
2012-11-01 11:11:01 +0700

    read2read-58 Разделы из аккаунта читателя открываются с ошибкой.

--- index.php
+++ index.php
@@ -5,5 +5,5 @@
 
 require_once(dirname(__FILE__).'/config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'dev', true);
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
 sfContext::createInstance($configuration)->dispatch();
